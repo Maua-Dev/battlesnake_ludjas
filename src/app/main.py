b7cf64aaaa22b4beb6940dde6bdaba23eb6d4c52
Luigi Trevisan <checkzin123@gmail.com>
2023-07-18 18:51:19 -0300

    info config + TU

--- src/app/main.py
+++ src/app/main.py
@@ -3,16 +3,28 @@ from mangum import Mangum
 
 app = FastAPI()
 
-# TODO: Implement my logic here to handle the requests from Battlesnake
+# GET / - info about the snake (https://docs.battlesnake.com/api/requests/info)
+# POST /start - start the game (https://docs.battlesnake.com/api/requests/start)
+# POST /move - move the snake (https://docs.battlesnake.com/api/requests/move)
+# POST /end - end the game (https://docs.battlesnake.com/api/requests/end)
+
 
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
+def info():
+    return {
+        "apiversion": "1",
+        "author": "LuigiTrevisan",
+        "color": "#8B0000",
+        "head": "tiger-king",
+        "tail": "hook",
+        "version": "1.0.0"
+    }
 
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int):
     return {"item_id": item_id}
+
 
 @app.post("/create_item")
 def create_item(request: dict):
@@ -20,7 +32,7 @@ def create_item(request: dict):
     name = request.get("name")
 
     return {"item_id": item_id,
-            "name": name}   
+            "name": name}
 
 
 handler = Mangum(app, lifespan="off")
--- tests/app/test_app.py
+++ tests/app/test_app.py
@@ -1,14 +1,21 @@
-from src.app.main import create_item, read_item, read_root
+from src.app.main import create_item, read_item, info
 
 
 class Test_App:
-    def test_read_root(self):
-        resp = read_root()
-        
-        assert resp == {"Hello": "World"}
+    def test_info(self):
+        resp = info()
+
+        assert resp == {
+            "apiversion": "1",
+            "author": "LuigiTrevisan",
+            "color": "#8B0000",
+            "head": "tiger-king",
+            "tail": "hook",
+            "version": "1.0.0"
+        }
 
     def test_get_item(self):
-        
+
         resp = read_item(1)
 
         assert resp == {"item_id": 1}
